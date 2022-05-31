9c60dcdcc6f731811e036fae52efe47801e32825
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-31 16:01:05 +0530

    [Harini]: Adding elements in stack

---
+++ StacksAndQueues/StacksAndQueues/MainClass.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace StacksAndQueues
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("--------------Welcome to concept of stackandqueue--------------");
+            LinkedListStack value = new LinkedListStack();
+            value.push(70);
+            value.push(30);
+            value.push(56);
+            value.DisPlay();
+
+
+        }
+    }
+}
